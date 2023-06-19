5f2c8a563e994aa24a11e964bf3b725bff1832a1
JASVEER KAUR <jasveerk1980@gmail.com>
2023-06-19 18:26:08 +0200

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BooksModule } from './books/books.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent 
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
