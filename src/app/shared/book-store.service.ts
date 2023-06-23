d1942c792a2828b9a09af52bd5b608bc35bbeb35
JASVEER KAUR <jasveerk1980@gmail.com>
2023-06-23 13:43:26 +0200

    Serach component with Subject

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -43,4 +43,7 @@ export class BookStoreService {
   remove(isbn: string): Observable<unknown>{
     return this.http.delete(`${this.apiUrl}/books/${isbn}`);
   }
+  getAllSearch(term: string): Observable<Book[]>{
+    return this.http.get<Book[]>(`${this.apiUrl}/books/search/`)
+  }
 }
